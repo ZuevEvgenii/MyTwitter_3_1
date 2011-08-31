673cbfa4fe031a9d62fa1219818860643768cf53
ZuevEvgenii <zuev.evgenii@gmail.com>
2011-08-31 12:14:28 +0600

    Chapter 13 done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,96 @@
+class UsersController < ApplicationController
+  before_filter :authenticate, :except => [:show, :new, :create]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
+  before_filter :registered, :only => [:new, :create]
+
+  def new
+    @user = User.new
+    @title = "Sign up"
+  end
+
+  def index
+    @title = "All users"
+    @users = User.paginate(:page => params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page])
+    @title = @user.name
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user != current_user
+      User.find(params[:id]).destroy
+      flash[:success] = "User destroyed."
+      redirect_to users_path
+    else
+      redirect_to users_path, :notice => "You can not delete youself"
+    end
+  end
+
+  def following
+    show_follow(:following)
+  end
+
+  def followers
+    show_follow(:followers)
+  end
+
+  def show_follow(action)
+    @title = action.to_s.capitalize
+    @user = User.find(params[:id])
+    @users = @user.send(action).paginate(:page => params[:page])
+    render 'show_follow'
+  end
+
+  private
+
+    def registered
+      redirect_to root_path, :notice => "You already sign in." if signed_in?
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      if current_user != nil
+        redirect_to(root_path) unless current_user.admin?
+      else
+        redirect_to(signin_path)
+      end
+    end
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp31::Application.config.secret_token = '4f4642ffd08df0acb8ce1510cbc8efa682d823f95486485389a4c316f78002b52e51a7580d68ec7583e8630569d29f453472df1790c3e9388b3eac99bee53b8c'
